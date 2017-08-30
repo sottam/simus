f4e6666d9ebe506b06e017bd28742359b6051f7d
sottam <alonsoaman@hotmail.com>
2017-08-30 09:50:09 -0300

    PWM teste comentado

--- Exemplos/pwm_teste.asm
+++ Exemplos/pwm_teste.asm
@@ -3,14 +3,15 @@
 ; Autor: Alonso Matos
 ; Data: 30/08/2017
 ;---------------------------------------------------
-    
+ORG 0
+;inicializa pino como pwm
     LDA #1
     STA PIN_TRAP
     LDA #2
     STA PIN_TRAP + 1
     LDA #101
     TRAP PIN_TRAP
-    
+;seta duty cycle em 128
     LDA #1
     STA PIN_TRAP
     LDA #128
@@ -18,9 +19,11 @@
     LDA #105
     TRAP PIN_TRAP
 
+    HLT
 
 
-PIN_TRAP DS 3
+
+PIN_TRAP: DS 3
 
 
 
