ab938bb85007850b1926751db3e24afed5682979
gpsilva2003 <gabriel.silva@gmail.com>
2017-09-06 20:39:13 -0300

    Update bip.asm

--- Exemplos/bip.asm
+++ Exemplos/bip.asm
@@ -1,13 +1,13 @@
-    ldi 6
-    trap freq
-
-    nop
-    hlt
-
-
-
-
-
+;--------------------------------------
+;  Ecoa um beep 
+;
+;--------------------------------------
+ORG 0
+    LDA #6
+    TRAP freq
+    NOP
+    HLT
 
 freq:   440
 dur:    1000
+END 0
