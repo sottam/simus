667818b1987bbd648e260f77fddcc82d734dc072
gpsilva2003 <gabriel.silva@gmail.com>
2017-09-06 20:35:33 -0300

    Update 26_letras.asm
    Atualizado

--- Exemplos/26_letras.asm
+++ Exemplos/26_letras.asm
@@ -1,33 +1,35 @@
-espera: ldi 1
-        trap 0
-        or #0
-        jz espera
-        sta letra
-loop0:
-        ldi 26
-        sta conta
+;---------------------------------------------------
+; Programa: Imprime o alfabeto na console
+; Autor: josé Antonio Borges
+; Data: 2016
+;---------------------------------------------------
+ORG 100
+letra:  DS 1
+conta:  DB 26
+cr:     DB 0dh
+lf:     DB 0ah
 
-loop:   ldi 2
-        trap letra
-        lda letra
-        add #1
-        sta letra
+ORG 0 
+espera: LDA #1       ; Lê uma letra inical da console
+        TRAP  0 
+        OR #0        
+        JZ espera     
+        STA letra    ; Armazena o valor lido
 
-        lda conta
-        sub #1
-        sta conta
-        jnz loop
+loop:   LDA #2       ; Fica em loop até conta = 0 
+        TRAP letra   ; Imprime o caractere na console
+        LDA letra
+        ADD #1
+        STA letra     ; Avança para a letra seguinte
+        LDA conta
+        SUB #1
+        STA conta
+        JNZ loop
         
-        ldi 2
-        trap cr
-        ldi 2
-        trap lf
+        LDA #2       ; Imprime retorno de carro  
+        TRAP cr
+        LDA #2
+        TRAP lf
+        HLT
 
-        hlt
-
-letra:  ds 1
-conta:  ds 1
-cr:     db 0dh
-lf:     db 0ah
-
-        end 0
+        END 0
