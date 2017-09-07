115589dc251ef19102727beed73976d5df4ad11c
gpsilva2003 <gabriel.silva@gmail.com>
2017-09-06 21:03:36 -0300

    Update rotina_banner.asm

--- Exemplos/rotina_banner.asm
+++ Exemplos/rotina_banner.asm
@@ -1,19 +1,16 @@
-; 
+; --------------------------------------------------------------------
 ; Descrição: Rotina para imprimir uma cadeia de caracteres no banner 
-; Parametros: O endereço da cadeia é passado na pilja
+; Parametros: O endereço da cadeia é passado na pilha
 ; Autor: Gabriel P. Silva
-; 
+; ---------------------------------------------------------------------
+ORG 300
 SP:    DW 0
 AUX:   DW 0
 PTR:   DW 0
-
 ;------------------------------------------------------
-
 ; constantes de hardware
 CLEARB   equ 3
 BANNER   equ 2
-
-
 ;------------------------------------------------------
 ROTINA:
     STS     SP
@@ -26,19 +23,15 @@ ROTINA:
     STA     PTR+1
     OUT     CLEARB
 loop:
-    lda     @ptr
-    or      #0
-    jz      fim
-    out     banner
-    lda     ptr
-    add     #1
-    sta     ptr
-    jmp     loop
+    LDA     @ptr
+    OR      #0
+    JZ      fim
+    OUT     banner
+    LDA     ptr
+    ADD     #1
+    STA     ptr
+    JMP     loop
 fim:
     LDS SP
     RET            
-  
-
-
-
-end
+END ROTINA
